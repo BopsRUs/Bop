0a257ab2b35843e55859cd63d3e6037a3082d33b
Justin Hurley <JustinHurley@users.noreply.github.com>
2021-01-25 18:33:41 -0600

    Initial Commit

---
+++ Bop/BopApp.swift
@@ -0,0 +1,17 @@
+//
+//  BopApp.swift
+//  Bop
+//
+//  Created by Justin Hurley on 1/25/21.
+//
+
+import SwiftUI
+
+@main
+struct BopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bop/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bop
+//
+//  Created by Justin Hurley on 1/25/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
