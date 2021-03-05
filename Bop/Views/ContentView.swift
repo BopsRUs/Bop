eeac4b08a0690e4873d11d16534a705bcbf50171
Justin Hurley <46689200+JustinHurley@users.noreply.github.com>
2021-03-04 18:46:51 -0600

    made the smallstockview work with navigationLinks, messed around with styling for a while

--- Bop/Views/ContentView.swift
+++ Bop/Views/ContentView.swift
@@ -7,14 +7,16 @@
 
 import SwiftUI
 let artists = ["Drake","Lady Gaga","Smino","The Who","Dolly Parton","Future","Taylor Swift","Clairo","Amine", "Pop Smoke", "The Beatles", "Mac Miller", "The Strokes", "Steely Dan", "Whitney", "Beach House", "The Pogues", "Doja Cat", "David Bowie", "Modest Mouse"]
+let totalPortfolioValue = "10,233.20"
 struct ContentView: View {
     var body: some View {
         NavigationView{
-            //Makes the large line graph at the top
-            LineView(data: [8,23,54,32,12,37,7,23,43],title:"Portfolio")
-                .padding(.bottom, 200)
-            Text("Overall: $10,301.21")
             ScrollView{
+                //Makes the large line graph at the top
+                LineView(data: [8,23,54,32,12,37,7,23,43])
+                    .padding(.bottom, 120)
+                    .padding(.top, 60)
+                    .navigationBarTitle("Portfolio \n $\(totalPortfolioValue)", displayMode: .large)
                 LazyVStack{
                     //loop generates the feed that shows a person's portfolio
                     ForEach(artists, id: \.self){ value in
@@ -23,10 +25,7 @@ struct ContentView: View {
                         Divider()
                         let smallStock = SmallStock(name: value, numShares: Float(numShares), delta: rand)
                         //view that handles the smaller individual stocks views
-                        Button(action:{
-                            StockView(stock: Stock(name: "\(smallStock.name)", delta: smallStock.delta))
-                            print("\(value) pressed!")
-                        }){
+                        NavigationLink(destination: StockView(stock: Stock(name: "\(smallStock.name)", delta: smallStock.delta))){
                             SmallStockView(smallStock: smallStock)
                                 //this sets all text color to black in the view
                                 .foregroundColor(.black)
@@ -34,7 +33,6 @@ struct ContentView: View {
                     }
                 }
             }
-            .padding(.top)
         }
     }
 }
